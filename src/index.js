312497ec9f6bbf2ca547b0eb5664fefea2633d91
stainpl <3tyokaha@gmail.com>
2025-09-08 16:13:14 +0100

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import 'bpk-stylesheets/base';
-import 'bpk-stylesheets/base.css';
-import 'bpk-stylesheets/font';
-import 'bpk-stylesheets/font.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "bpk-stylesheets/base.css"; 
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
